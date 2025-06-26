157f955f1ba62e3d3ca1f425425e8bc8a0219aa4
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-26 14:57:56 +0530

    removed member

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,10 +8,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Student student = new Student();
-        student.setId(sc.nextInt());
-        String temp =  sc.nextLine();
-        student.setName(sc.nextLine());
-        student.setAge(sc.nextInt());
+        student.setId(20);
+        student.setName("Joe");
+        student.setAge(20);
 
         Configuration config = new Configuration();
 
@@ -23,9 +22,17 @@ public class Main {
         Session session=sessionFactory.openSession();
 
         Transaction transaction= session.beginTransaction();
-        session.persist(student);
+
+        //insertion of data in DB
+//        session.persist(student);
+//        transaction.commit();
+//        System.out.println(student);
+
+        //read class
+        Student s1= session.find(com.example.Student.class, student.getId());
+        session.remove(s1);
         transaction.commit();
-        System.out.println(student);
+        System.out.println(s1.toString());
         session.close();
         sessionFactory.close();
     }
